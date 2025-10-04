12ae7311edb5f9dfb3eeabd56e87a6522085fb2e
ishayasolo <ishaya808@gmail.com>
2025-10-04 03:40:45 +0100

    feat: add template components

---
+++ src/components/templates/search-page-template.tsx
@@ -0,0 +1,14 @@
+import { SearchHeader } from "@/components/organisms/search-header";
+
+/**
+ * Template: SearchPageTemplate - Main page layout for Pokémon search
+ */
+export function SearchPageTemplate() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100">
+      <div className="container mx-auto px-4 py-8">
+        <SearchHeader />
+      </div>
+    </div>
+  );
+}
